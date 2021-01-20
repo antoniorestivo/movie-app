1c85a373d6a2687f73b68fd737e15afb9212d72f
antoniorestivo <nonebesides370@gmail.com>
2021-01-20 09:34:36 -0700

    Unfinished homework needs testing and help

--- app/controllers/api/movies_controller.rb
+++ app/controllers/api/movies_controller.rb
@@ -1,30 +1,12 @@
-require "http"
+
 class Api::MoviesController < ApplicationController
-  def see_all_movies
-    
-    @all_movies = Movie.all
-    @descending_movies = Movie.order(title :desc)
-    render "movie.json.jb"
-
-    all_movies_results = HTTP.get("http://localhost:3000/api/all_movie_list")
-  puts all_movies_results
   
+  def index #get all movies
+    @movie = Movie.all
+    render index.json.jb
   end
-
-  def choose_a_movie_query_parameter
-
-     user_choice = params["single_movie"]
-     @user_movie = Movie.find_by(title: user_choice)
-     select_movies_results = HTTP.get("http://localhost:3000/api/select_movie_list")
-     puts select_movies_results
-     render "select_movie.json.jb"
-   end
-   def choose_a_movie_segment_parameter
-      user_input = params[:wildcard]
-      @user_segment_movie = Movie.find_by(title: user_input)
-      render "select_query_movie.json.jb"
     
-   end
+  end
 end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,15 @@
 Rails.application.routes.draw do
   
-  namespace :api do
+    
     get "/actor_list" => "actors#sample_action"
-  
 
-  namespace :api do
-    get "/all_movie_list" => "movies#see_all_movies"
-  
+    get "/movies" => "movies#index"
 
-  namespace :api do
-    get "/select_movie_list" => "movies#choose_a_movie_query_parameter"
+    
 
-    namespace :api do
-      get "/segment_params_path/:wildcard" =>
-      "movies#choose_a_movie_segment_parameter"
-
-    end
-  
+    
+    
+  end 
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-end
+
