dd07848315f1f8207bf9853cb4f0194c0ba8c753
antoniorestivo <nonebesides370@gmail.com>
2021-01-22 17:48:28 -0700

    your commit message goes here

--- app/controllers/api/movies_controller.rb
+++ app/controllers/api/movies_controller.rb
@@ -16,6 +16,26 @@ class Api::MoviesController < ApplicationController
     @movie.save
     render json: @movie
   end
+  def show
+    @movie = Movie.find_by(id: params[:id])
+    render json: @movie
+  end
+  def update
+   @movie = Movie.find_by(id: params[:id])
+   @movie.title = params['title'] || @movie.title
+   @movie.year = params['year'] || @movie.year
+   @movie.plot = params['plot'] || @movie.plot
+   @movie.director = params['director'] || @movie.director
+   @movie.english = params['english'] || @movie.english
+   @movie.save
+   render json: @movie
+  end
+  def destroy
+    @movie = Movie.find_by(id: params[:id])
+    @movie.destroy
+    render json: {message: "Deleted movie"}
+    
+  end
   
     
   end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
     get '/actor_list' => 'actors#sample_action'
     get '/movies' => 'movies#index'
     post '/movies' => 'movies#create'
+    get '/movies/:id' => 'movies#show'
+    patch '/movies/:id' =>'movies#update'
+    delete '/movies/:id' => 'movies#destroy'
   end
 
     
